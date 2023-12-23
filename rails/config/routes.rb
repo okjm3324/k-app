f6a3ebc0d72864b2f082e7811cf4d84b6a70b633
okjm3324 <okjm.yuma@gmail.com>
2023-12-24 00:56:52 +0900

    Add:認証機能・Userのテスト
    deviseを使用し、認証機能を追加
    User新規作成、認証のテストを追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
+
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
   # Defines the root path route ("/")
