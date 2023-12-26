8b541444da19db327166b44d65b20b0630cb1ff1
okjm3324 <okjm.yuma@gmail.com>
2023-12-26 22:16:29 +0900

    ADD:フロント完成

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -3,10 +3,12 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import { AppProps } from 'next/app'
 import * as React from 'react'
-
+import Snackbar from '@/components/Snackbar'
+import '@/styles/destyle.css'
 import createEmotionCache from '@/styles/createEmotionCache'
 import theme from '@/styles/theme'
-
+import CurrentUserFetch from '@/components/CurrentUserFetch'
+import Header from '@/components/Header'
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
 
@@ -21,7 +23,10 @@ export default function MyApp(props: MyAppProps): JSX.Element {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <CurrentUserFetch />
+        <Header />
         <Component {...pageProps} />
+        <Snackbar />
       </ThemeProvider>
     </CacheProvider>
   )
